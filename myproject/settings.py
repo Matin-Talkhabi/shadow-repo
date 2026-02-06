777eb2c60e101363c6129c6b1dc31e341700f4ca
Matin-Talkhabi <matintalkhabidev@gmail.com>
2026-02-07 01:54:44 +0330

    show sir shadow

--- myproject/settings.py
+++ myproject/settings.py
@@ -41,7 +41,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 LANGUAGE_CODE = 'en-us'
 
